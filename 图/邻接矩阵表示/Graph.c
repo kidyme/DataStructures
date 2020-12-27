ac96ff874ee42009081f95e17da540ddf8e3865a
kidyme <1554768198@qq.com>
2020-12-27 15:22:24 +0800

    提交图的邻接矩阵表示

---
+++ 图/邻接矩阵表示/Graph.c
@@ -0,0 +1,375 @@
+//
+// Created by kidyme on 2020/12/2.
+//
+
+#include "Graph.h"
+
+void BreadthFirstSearch(GraphPtr graph) {
+    printf("广度优先搜索: ");
+
+    QueuePtr assist_queue = InitQueue(graph->vertex_num + 1);
+    int assist_array[graph->vertex_num + 1];
+    // 初始化辅助数组
+    for (int i = 1; i <= graph->vertex_num; ++i) {
+        assist_array[i] = 0;
+    }
+
+    for (int i = 1; i <= graph->vertex_num; ++i) {
+        if (!assist_array[i]) {
+            EnQueue(assist_queue, graph->vertexes[i]);// 若某顶点未被访问，则将其入队
+            assist_array[i] = 1;
+            while (!IsEmpty(assist_queue)) {
+                char tmp;
+                int tmp_index;
+                int next;
+                DeQueue(assist_queue, &tmp);// 出队访问，并将与其有边的点依次入队
+                printf("%c ", tmp);
+                LocateVertex(graph, tmp, &tmp_index);
+
+                while ((next = GetNextVertex(graph, tmp_index, assist_array))) {
+                    EnQueue(assist_queue, graph->vertexes[next]);
+                    assist_array[next] = 1;
+                }
+            }
+        }
+    }
+    printf("\n");
+}
+
+void DepthFirstSearch(GraphPtr graph) {
+    printf("深度优先搜索: ");
+
+    int assist_array[graph->vertex_num + 1];
+    // 初始化辅助数组
+    for (int i = 1; i <= graph->vertex_num; ++i) {
+        assist_array[i] = 0;
+    }
+
+    for (int i = 1; i <= graph->vertex_num; ++i) {
+        if (!assist_array[i]) {
+            DFS(graph, i, assist_array);
+        }
+    }
+    printf("\n");
+}
+
+void DFS(GraphPtr graph, int move_vertex, int assist_array[]) {
+    printf("%c ", graph->vertexes[move_vertex]);
+    assist_array[move_vertex] = 1;
+
+    int next = GetNextVertex(graph, move_vertex, assist_array);
+    while (next) {
+        DFS(graph, next, assist_array);
+        next = GetNextVertex(graph, move_vertex, assist_array);
+    }
+}
+
+int GetNextVertex(GraphPtr graph, int vertex, int assist_array[]) {
+    for (int col = 1; col <= graph->vertex_num; ++col) {
+        // 顶点vertex到顶点col有边 且 辅助数组中顶点col未被访问
+        if (graph->arcs[vertex][col].adj && !assist_array[col]) {
+            return col;
+        }
+    }
+    return 0;
+}
+
+// 初始化队列
+QueuePtr InitQueue(int length) {
+    QueuePtr queue = (QueuePtr) malloc(sizeof(Queue));
+    queue->front = 0;
+    queue->rear = 0;
+    queue->max_length = length;
+    queue->length = 0;
+
+    return queue;
+}
+
+// 判断队是否为空
+int IsEmpty(QueuePtr queue) {
+    return (queue->length == 0);
+}
+
+// 判断队是否已满
+int IsFull(QueuePtr queue) {
+    return (queue->length == queue->max_length);
+}
+
+// 入队
+void EnQueue(QueuePtr queue, char value) {
+    if (IsFull(queue)) {
+        printf("队满，出错\n");
+        return;
+    }
+
+    queue->length++;
+    queue->rear++;
+    queue->data[queue->rear] = value;
+}
+
+// 出队
+void DeQueue(QueuePtr queue, char *value) {
+    if (IsEmpty(queue)) {
+        printf("队空，出错\n");
+    }
+
+    queue->length--;
+    *value = queue->data[queue->front + 1];
+    queue->front++;
+}
+
+void PrintQueue(QueuePtr queue) {
+    for (int i = (queue->front + 1); i <= queue->rear; ++i) {
+        printf("%c ", queue->data[i]);
+    }
+    printf("\n");
+}
+
+void LocateVertex(GraphPtr graph, char vertex, int *index) {
+    int tmp = 1;
+    for (; tmp < MAX_VERTEX_NUM; ++tmp) {
+        if (vertex == graph->vertexes[tmp]) {
+            *index = tmp;
+        }
+    }
+}
+
+int CreateGraph(GraphPtr graph, GraphKind kind) {
+    printf("请输入图的顶点数: ");
+    scanf("%d", &graph->vertex_num);
+    fflush(stdin);
+    printf("请输入图的边数: ");
+    scanf("%d", &graph->edge_num);
+    fflush(stdin);
+
+    switch (kind) {
+        case DG:
+            CreateDGGraph(graph);
+            break;
+        case DN:
+            CreateDNGraph(graph);
+            break;
+        case UDG:
+            CreateUDGGraph(graph);
+            break;
+        case UDN:
+            CreateUDNGraph(graph);
+            break;
+        default:
+            return ERROR;
+    }
+
+    return SUCCESS;
+}
+
+int CreateDGGraph(GraphPtr graph) {
+    // 输入顶点集合
+    for (int i = 1; i <= graph->vertex_num; ++i) {
+        printf("请输入第%d个顶点: ", i);
+        scanf("%c", &graph->vertexes[i]);
+        fflush(stdin);
+    }
+
+    // 初始化邻接矩阵
+    for (int i = 1; i <= graph->vertex_num; ++i) {
+        for (int j = 1; j <= graph->vertex_num; ++j) {
+            graph->arcs[i][j].adj = 0;
+            graph->arcs[i][j].info = NULL;
+        }
+    }
+
+    // 赋值邻接矩阵
+    for (int i = 1; i <= graph->edge_num; ++i) {
+        char start_vex, end_vex;
+        int row = 0, col = 0;
+
+        printf("请输入第%d条边的起点和终点: ", i);
+        scanf("%c %c", &start_vex, &end_vex);
+        fflush(stdin);
+
+        LocateVertex(graph, start_vex, &row);
+        LocateVertex(graph, end_vex, &col);
+
+        while (row == 0) {
+            printf("起点不存在!\n");
+            printf("请重新输入第%d条边的起点: ", i);
+            scanf("%c", &start_vex);
+            fflush(stdin);
+            LocateVertex(graph, start_vex, &row);
+        }
+        while (col == 0) {
+            printf("终点不存在!\n");
+            printf("请重新输入第%d条边的终点: ", i);
+            scanf("%c", &end_vex);
+            fflush(stdin);
+            LocateVertex(graph, end_vex, &col);
+        }
+
+        graph->arcs[row][col].adj = 1;
+    }
+
+
+    return SUCCESS;
+}
+
+int CreateDNGraph(GraphPtr graph) {
+    // 输入顶点集合
+    for (int i = 1; i <= graph->vertex_num; ++i) {
+        printf("请输入第%d个顶点: ", i);
+        scanf("%c", &graph->vertexes[i]);
+        fflush(stdin);
+    }
+
+    // 初始化邻接矩阵
+    for (int i = 1; i <= graph->vertex_num; ++i) {
+        for (int j = 1; j <= graph->vertex_num; ++j) {
+            graph->arcs[i][j].adj = 0;
+            graph->arcs[i][j].info = NULL;
+        }
+    }
+
+    // 赋值邻接矩阵
+    for (int i = 1; i <= graph->edge_num; ++i) {
+        char start_vex, end_vex;
+        int row = 0, col = 0;
+
+        printf("请输入第%d条边的起点和终点: ", i);
+        scanf("%c %c", &start_vex, &end_vex);
+        fflush(stdin);
+
+        LocateVertex(graph, start_vex, &row);
+        LocateVertex(graph, end_vex, &col);
+
+        while (row == 0) {
+            printf("起点不存在!\n");
+            printf("请重新输入第%d条边的起点: ", i);
+            scanf("%c", &start_vex);
+            fflush(stdin);
+            LocateVertex(graph, start_vex, &row);
+        }
+        while (col == 0) {
+            printf("终点不存在!\n");
+            printf("请重新输入第%d条边的终点: ", i);
+            scanf("%c", &end_vex);
+            fflush(stdin);
+            LocateVertex(graph, end_vex, &col);
+        }
+
+        printf("请输入第%d条边的权值: ", i);
+        int weight = 0;
+        scanf("%d", &weight);
+        fflush(stdin);
+
+        graph->arcs[row][col].adj = weight;
+    }
+
+
+    return SUCCESS;
+}
+
+int CreateUDGGraph(GraphPtr graph) {
+    // 输入顶点集合
+    for (int i = 1; i <= graph->vertex_num; ++i) {
+        printf("请输入第%d个顶点: ", i);
+        scanf("%c", &graph->vertexes[i]);
+        fflush(stdin);
+    }
+
+    // 初始化邻接矩阵
+    for (int i = 1; i <= graph->vertex_num; ++i) {
+        for (int j = 1; j <= graph->vertex_num; ++j) {
+            graph->arcs[i][j].adj = 0;
+            graph->arcs[i][j].info = NULL;
+        }
+    }
+
+    // 赋值邻接矩阵
+    for (int i = 1; i <= graph->edge_num; ++i) {
+        char start_vex, end_vex;
+        int row = 0, col = 0;
+
+        printf("请输入第%d条边的两个点: ", i);
+        scanf("%c %c", &start_vex, &end_vex);
+        fflush(stdin);
+
+        LocateVertex(graph, start_vex, &row);
+        LocateVertex(graph, end_vex, &col);
+
+        while (row == 0) {
+            printf("第一个点不存在!\n");
+            printf("请重新输入第%d条边的第一个点: ", i);
+            scanf("%c", &start_vex);
+            fflush(stdin);
+            LocateVertex(graph, start_vex, &row);
+        }
+        while (col == 0) {
+            printf("第二个点不存在!\n");
+            printf("请重新输入第%d条边的第二个点: ", i);
+            scanf("%c", &end_vex);
+            fflush(stdin);
+            LocateVertex(graph, end_vex, &col);
+        }
+        graph->arcs[row][col].adj = 1;
+        graph->arcs[col][row].adj = 1;
+    }
+
+
+    return SUCCESS;
+}
+
+
+int CreateUDNGraph(GraphPtr graph) {
+
+    // 输入顶点集合
+    for (int i = 1; i <= graph->vertex_num; ++i) {
+        printf("请输入第%d个顶点: ", i);
+        scanf("%c", &graph->vertexes[i]);
+        fflush(stdin);
+    }
+
+    // 初始化邻接矩阵
+    for (int i = 1; i <= graph->vertex_num; ++i) {
+        for (int j = 1; j <= graph->vertex_num; ++j) {
+            graph->arcs[i][j].adj = 0;
+            graph->arcs[i][j].info = NULL;
+        }
+    }
+
+    // 赋值邻接矩阵
+    for (int i = 1; i <= graph->edge_num; ++i) {
+        char start_vex, end_vex;
+        int row = 0, col = 0;
+
+        printf("请输入第%d条边的两个点: ", i);
+        scanf("%c %c", &start_vex, &end_vex);
+        fflush(stdin);
+
+        LocateVertex(graph, start_vex, &row);
+        LocateVertex(graph, end_vex, &col);
+
+        while (row == 0) {
+            printf("第一个点不存在!\n");
+            printf("请重新输入第%d条边的第一个点: ", i);
+            scanf("%c", &start_vex);
+            fflush(stdin);
+            LocateVertex(graph, start_vex, &row);
+        }
+        while (col == 0) {
+            printf("第二个点不存在!\n");
+            printf("请重新输入第%d条边的第二个点: ", i);
+            scanf("%c", &end_vex);
+            fflush(stdin);
+            LocateVertex(graph, end_vex, &col);
+        }
+
+        printf("请输入第%d条边的权值: ", i);
+        int weight = 0;
+        scanf("%d", &weight);
+        fflush(stdin);
+
+        graph->arcs[row][col].adj = weight;
+        graph->arcs[col][row].adj = weight;
+    }
+
+    return SUCCESS;
+}
---
+++ 图/邻接矩阵表示/Graph.h
@@ -0,0 +1,101 @@
+//
+// Created by kidyme on 2020/12/2.
+//
+// 邻接矩阵表示法
+// AdjacencyMatrix
+// 带权图的权值类型为int
+// 将弧也称作边
+//
+
+#ifndef PRAC_C_GRAPH_H
+#define PRAC_C_GRAPH_H
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+//#define INFINITY INT_MAX // 定义+∞
+#define MAX_VERTEX_NUM 21// 最多20个顶点
+#define SUCCESS 1
+#define ERROR 0
+
+typedef enum {
+    DG = 1,// 有向图
+    DN,    // 有向网(有向权图)
+    UDG,   // 无向图
+    UDN    // 无向网(无向权图)
+} GraphKind;
+
+typedef struct {
+    int adj;   // 若为图，则adj取值0或1表示是否邻接；若为网则表示权值大小，非邻接取值0
+    char *info;// 指向该边相关信息的指针
+} ArcCell, AdjacencyMatrix[MAX_VERTEX_NUM][MAX_VERTEX_NUM];
+
+typedef struct {
+    GraphKind kind;               // 图的类型
+    int vertex_num;               // 当前图的顶点数
+    int edge_num;                 // 当前图的边数
+    char vertexes[MAX_VERTEX_NUM];// 顶点集合(顶点向量)
+    AdjacencyMatrix arcs;         // 邻接矩阵
+} Graph, *GraphPtr;
+
+void LocateVertex(GraphPtr graph, char vertex, int *index);
+// 取得顶点在顶点集合中的位置
+
+int CreateGraph(GraphPtr graph, GraphKind kind);
+// 图构造
+
+int CreateDGGraph(GraphPtr graph);
+// 有向图构造
+
+int CreateDNGraph(GraphPtr graph);
+// 有向网构造
+
+int CreateUDGGraph(GraphPtr graph);
+// 无向图构造
+
+int CreateUDNGraph(GraphPtr graph);
+// 无向网构造
+
+//----------深度优先搜索----------------------------------------------
+
+void DepthFirstSearch(GraphPtr graph);
+// 深度优先搜索
+
+void DFS(GraphPtr graph, int move_vertex, int assist_array[]);
+// 深度优先搜索的递归函数
+
+int GetNextVertex(GraphPtr graph, int vertex, int assist_array[]);
+// 返回图中某一顶点的下一个顶点在顶点集合中的下标，需保证下一个顶点未被访问过，若已无顶点则返回0
+
+
+//----------广度优先搜索----------------------------------------------
+
+void BreadthFirstSearch(GraphPtr graph);
+// 广度优先搜索
+
+
+typedef struct queue {
+    int front;
+    int rear;
+    int max_length;
+    int length;
+    char data[MAX_VERTEX_NUM + 1];
+    int flag;// 上次出队前，是否有过空队入队
+} Queue, *QueuePtr;
+// 辅助队列
+// 队首下标指向队首元素的上一位
+QueuePtr InitQueue(int length);
+// 初始化队列
+int IsEmpty(QueuePtr queue);
+// 判断队是否为空
+int IsFull(QueuePtr queue);
+// 判断队是否已满
+void EnQueue(QueuePtr queue, char value);
+// 入队
+void DeQueue(QueuePtr queue, char *value);
+// 出队
+void PrintQueue(QueuePtr queue);
+// 打印队列
+
+
+#endif//PRAC_C_GRAPH_H
